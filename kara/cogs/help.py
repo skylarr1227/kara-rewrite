e5dd78e822c09b88411d6a7d11071492ad0e4159
Stefan <stefankar1000@gmail.com>
2019-03-07 01:43:48 +0100

    Update cogs
    Add help, utility
    Remove test, fun

---
+++ kara/cogs/help.py
@@ -0,0 +1,42 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot, Context, Cog
+
+
+class Help(Cog):
+    _bot: Bot
+
+    def __init__(self, bot):
+        self._bot = bot
+
+    @commands.command(name="help", aliases=["h"], brief="Shows all available commands.", hidden=True)
+    async def help(self, ctx: Context, name: str = None):
+        """Shows all available commands."""
+        embed: discord.Embed = discord.Embed(title="Help")
+        command: discord.ext.commands.Command
+
+        embed.set_footer(text=ctx.message.author.name, icon_url=ctx.message.author.avatar_url)
+
+        if not name:  # all commands, no args
+            for command in self._bot.commands:
+                if command.hidden:
+                    continue
+                embed.add_field(name=command.name, value=command.brief, inline=False)
+            return await ctx.send(embed=embed)
+
+        if name not in [i.name for i in self._bot.commands]:  # invalid command name given
+            return await ctx.send(f"Command **{name}** not found. Type `{self._bot.command_prefix}help` for more info.")
+
+        command = next(i for i in self._bot.commands if i.name == name)
+        if command.hidden:  # command is hidden
+            return await ctx.send(f"Help for **{name}** not found. Type `{self._bot.command_prefix}help` for more info.")
+
+        embed.title = f"Help for the {command.name} command"
+        if command.aliases:  # command has aliases
+            embed.add_field(name="Aliases", value=command.aliases)
+        embed.add_field(name="Description", value=command.help.replace("{prefix}", self._bot.command_prefix))
+        await ctx.send(embed=embed)
+
+
+def setup(bot: Bot):
+    bot.add_cog(Help(bot))
---
+++ kara/cogs/utility.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot, Context, Cog
+from typing import Optional
+
+
+class Utility(Cog):
+    _bot: Bot
+
+    def __init__(self, bot):
+        self._bot = bot
+
+    @commands.command(name="avatar", aliases=["a", "pfp"], brief="Shows a user's avatar.")
+    async def avatar(self, ctx: Context, user: Optional[discord.User], *args: Optional[str]):
+        """Shows a user's avatar.
+        This command accepts these formats:\n
+        **Mention**:` {prefix}avatar @Predator`\n
+        **Name**:` {prefix}avatar Predator`\n
+        **ID**:` {prefix}avatar 195935967440404480`\n
+        Note: You can get an avatar of a user not in this guild by using their user ID."""
+        if not user:
+            user = await self._bot.get_user_info(args[0])
+
+        embed: discord.Embed = discord.Embed(description=f"{user.mention}'s avatar")
+        embed.set_image(url=user.avatar_url_as(size=1024))
+        embed.set_footer(text=ctx.message.author.name, icon_url=ctx.message.author.avatar_url)
+        await ctx.send(embed=embed)
+
+
+def setup(bot: Bot):
+    bot.add_cog(Utility(bot))
